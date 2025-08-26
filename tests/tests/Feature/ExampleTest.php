3a81d3329965d693450314b2b583fb83b1e33608
Gustavo Canuto <gcanuto.dev@gmail.com>
2025-08-25 23:05:29 -0300

    feat: criado teste intregado com banco de dados separado env.testing

---
+++ tests/tests/Feature/ExampleTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     */
+
+    use RefreshDatabase;
+
+    public function test_the_application_returns_a_successful_response(): void
+    {
+        User::factory()->create();
+
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
